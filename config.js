37f4524ad178a637a6216be97c9144fc506d1765
Jai Tummalacherla <95111568+Jai-Tummalacherla@users.noreply.github.com>
2023-11-26 09:33:09 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC9_xfTtGsup0ZwDjDCNAYm9iDioiGl7SE",
+  authDomain: "story-telling-app-login-48f74.firebaseapp.com",
+  projectId: "story-telling-app-login-48f74",
+  storageBucket: "story-telling-app-login-48f74.appspot.com",
+  messagingSenderId: "926419948833",
+  appId: "1:926419948833:web:382265f57a5264a7719a60"
+}
